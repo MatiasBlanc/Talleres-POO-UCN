4ec751d226a40db4215a53364e28750cd5fecdbe
Matias Blanc <matias.f.blanc@gmail.com>
2026-09-24 00:11:22 -0300

    developed cargarArchivos

--- taller1/src/taller1/Main.java
+++ taller1/src/taller1/Main.java
@@ -4,6 +4,8 @@
 
 package taller1;
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
@@ -31,7 +33,40 @@ public class Main {
     		mostrarMenu();
             opcion = teclado.nextInt();
             
-            
+            switch (opcion) {
+
+            case 1:
+                cargarArchivos();
+                break;
+
+            case 2:
+                procesarSolicitudes();
+                break;
+
+            case 3:
+                inscripcionManual();
+                break;
+
+            case 4:
+                administrarCurso();
+                break;
+
+            case 5:
+                generarReportes();
+                break;
+
+            case 6:
+                mostrarEstadisticas();
+                break;
+
+            case 7:
+                System.out.println("Saliendo del programa...");
+                break;
+
+            default:
+                System.out.println("Opcion invalida.");
+        }
+
     	}
     }
     
@@ -46,5 +81,88 @@ public class Main {
                 6) Analisis estadistico
                 7) Salir
                 """);
+    }
+    
+    static void cargarArchivos() {
+
+        cantidadAlumnos = 0;
+        cantidadSolicitudes = 0;
+
+        // ---------- CARGAR ALUMNOS ----------
+        File archivoAlumnos = new File("Alumnos.txt");
+
+        try {
+            Scanner lectorAlumnos = new Scanner(archivoAlumnos);
+
+            while (lectorAlumnos.hasNextLine() && cantidadAlumnos < MAX) {
+
+                String linea = lectorAlumnos.nextLine();
+                String[] datos = linea.split(";");
+
+                if (datos.length == 4) {
+                    nombresAlumnos[cantidadAlumnos] = datos[0];
+                    apellidosAlumnos[cantidadAlumnos] = datos[1];
+                    rutsAlumnos[cantidadAlumnos] = datos[2];
+                    paralelosAlumnos[cantidadAlumnos] = datos[3];
+
+                    cantidadAlumnos++;
+                }
+            }
+
+            lectorAlumnos.close();
+
+        } catch (IOException e) {
+            System.out.println("No se pudo leer Alumnos.txt");
+        }
+
+
+        // ---------- CARGAR SOLICITUDES ----------
+        File archivoSolicitudes = new File("Solicitudes.txt");
+
+        try {
+            Scanner lectorSolicitudes = new Scanner(archivoSolicitudes);
+
+            while (lectorSolicitudes.hasNextLine() && cantidadSolicitudes < MAX) {
+
+                String linea = lectorSolicitudes.nextLine();
+                String[] datos = linea.split("-");
+
+                if (datos.length == 2) {
+                    nombresSolicitudes[cantidadSolicitudes] = datos[0];
+                    apellidosSolicitudes[cantidadSolicitudes] = datos[1];
+
+                    cantidadSolicitudes++;
+                }
+            }
+
+            lectorSolicitudes.close();
+
+        } catch (IOException e) {
+            System.out.println("No se pudo leer Solicitudes.txt");
+        }
+
+
+        System.out.println("Alumnos cargados: " + cantidadAlumnos);
+        System.out.println("Solicitudes cargadas: " + cantidadSolicitudes);
+    }
+
+    static void procesarSolicitudes() {
+        System.out.println("Procesando solicitudes...");
+    }
+
+    static void inscripcionManual() {
+        System.out.println("Inscripcion manual...");
+    }
+
+    static void administrarCurso() {
+        System.out.println("Administracion del curso...");
+    }
+
+    static void generarReportes() {
+        System.out.println("Generando reportes...");
+    }
+
+    static void mostrarEstadisticas() {
+        System.out.println("Analisis estadistico...");
     }
 }
